a812cde32f43e1799fe0601be7f8dc168eaa39e0
33Irina <zyryanovai17@gmail.com>
2022-11-10 13:33:11 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 15_325;
+
+        int miles = price / 20;
+        System.out.println("Бонусные мили:" + miles);
+    }
+}
